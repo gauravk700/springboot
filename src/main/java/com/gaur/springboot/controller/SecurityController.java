32fee1f71d735e76b7066f8876e77d4d8292bed4
Gaurav Kumar <gauravkumar@Mac.lan>
2025-01-11 11:04:14 +0530

    Added security and csrf token

---
+++ src/main/java/com/gaur/springboot/controller/SecurityController.java
@@ -0,0 +1,21 @@
+package com.gaur.springboot.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecurityController {
+
+    @GetMapping("sessionId")
+    public String getSessionId(HttpServletRequest httpServletRequest){
+        return "getSessionId  - " + httpServletRequest.getSession().getId();
+    }
+
+    @GetMapping("csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest httpServletRequest){
+        return (CsrfToken) httpServletRequest.getAttribute("_csrf");
+    }
+}
