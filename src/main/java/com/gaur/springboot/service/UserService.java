602a50053175a2ba563af59ddbcce698c9efdc39
Gaurav Kumar <gauravkumar@Mac.lan>
2025-01-12 19:39:24 +0530

    Added Bcrpyt password encoder

--- src/main/java/com/gaur/springboot/service/UserService.java
+++ src/main/java/com/gaur/springboot/service/UserService.java
@@ -3,6 +3,7 @@ package com.gaur.springboot.service;
 import com.gaur.springboot.model.Users;
 import com.gaur.springboot.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,11 +15,14 @@ public class UserService {
     @Autowired
     private UserRepository userRepo;
 
+    private final BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(12);
+
     public List<Users> getAllUsers(){
         return userRepo.findAll();
     }
 
     public Users createUser(Users user){
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         return userRepo.save(user);
     }
 }
